e9b341d1e49226550eac6db104028011e2b739eb
 抹茶味栗子酱 <36477648+NatsunoZero@users.noreply.github.com>
2020-07-25 22:37:21 +0800

    Create googleanalysis.js

---
+++ js/Google/googleanalysis.js
@@ -0,0 +1,9 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118640962-1"></script>');
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-118640962-1');
